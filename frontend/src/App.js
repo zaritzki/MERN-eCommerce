42d7aebbe16f09bb73ff81e771eefcf76c083c02
zaritzki <zars195@gmail.com>
2020-10-21 17:47:31 +0100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+
+// function App() {
+
+// arrow functions
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to ProShop</h1>
+    </>
+  ); 
+}
+
+export default App;
